df88391bdb3712bc06156e15e99c8bb27cf0e80f
piasmajumdar <piasmajumdar@gmail.com>
2026-04-05 11:24:58 +0530

    practiced router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello from react</div>
+  },
+  
+])
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
