3faaa52a2101a50b75b09d249d5f6b5b4d98ec62
piasmajumdar <piasmajumdar@gmail.com>
2026-04-06 23:23:18 +0530

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import App from './App';
+
 
 
 const router = createBrowserRouter([
@@ -9,7 +11,23 @@ const router = createBrowserRouter([
     path: "/",
     element: <div>Hello from react</div>
   },
-  
+  {
+    path: "about",
+    element: <div>About me here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All blogs are here</div>
+  },
+  {
+    path: 'app',
+    Component: App,
+    // element: <App></App>
+  },
+  {
+    path: 'app2',
+    element: <App></App>
+  }
 ])
 
 
