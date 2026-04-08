711897bb28bf9721700f36add8b2f75277af0866
piasmajumdar <piasmajumdar@gmail.com>
2026-04-08 20:40:26 +0530

    link, navlink, aside, sidebar

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Thank you for visiting our website</small></p>
+            <a href="/">terms and condition</a>
+            <a href="/">Social & Privacy</a>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
+import "./Header.css";
 
 const Header = () => {
     return (
         <div>
             <h3>This is Header</h3>
+            <nav>
+                {/* <a href="/">Home</a>
+                <a href="/mobiles">Mobiles</a>
+                <a href="/laptops">Laptops</a> */}
+
+                {/* <Link to="/">Home</Link>
+                <Link to="/mobiles">Mobiles</Link>
+                <Link to="/laptops">Laptops</Link> */}
+
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/mobiles">Mobiles</NavLink>
+                <NavLink to="/laptops">Laptops</NavLink>
+            </nav>
         </div>
     );
 };
