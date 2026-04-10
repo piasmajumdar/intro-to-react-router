ccb0237f6fae82641774f0e8b5780f244e370525
piasmajumdar <piasmajumdar@gmail.com>
2026-04-10 19:34:15 +0530

    Navigate component to return into another page, location

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,10 +1,15 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({ user }) => {
     const [showInfo, setShowInfo] = useState(false);
+    const [visitHome, setVisitHome] = useState(false);
+    
     const { id, name, email, phone } = user;
+
+    const location = useLocation();
+    console.log(location);
 
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=> res.json());
 
@@ -14,6 +19,11 @@ const User = ({ user }) => {
         padding: '10px',
         margin: '10px'
     }
+
+    if(visitHome) {
+        return <Navigate to='/'></Navigate>
+    }
+
     return (
         <div style={userStyle}>
             <h3>{name}</h3>
@@ -26,6 +36,7 @@ const User = ({ user }) => {
                     <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
             }
+            <button onClick={()=> setVisitHome(true)}>Visit Home</button>
 
         </div>
     );
